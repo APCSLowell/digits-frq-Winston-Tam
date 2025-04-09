645ce56cbde0c7c5f705281090f2dd5b350db6fd
Winston-Tam <winstontam28@gmail.com>
2025-04-09 11:59:31 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -12,18 +12,17 @@ public class Digits
 		digitList.add(0);
 		int n = num;
 		while (n > 0){
-		digitList.add(0, n%10);
+		digitList.add(0,n%10);
 		n = n/10;
 		}
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-	for (int i = 0; i < digitList.size()-1; i++){
-	if (digitList.get(i) >= digitList.get(i+1))
+	for (int i = 0; i < digitList.size()-1; i++)
+	if (digitList.get(i+1) <= digitList.get(i))
 		return false;
 	return true;
-	}
 	}
 	
 	public String toString()
